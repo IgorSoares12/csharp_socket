6d5503a486bd0725437142bcddc320cfc33d401c
igorsoares12 <joseigorsoarespenha@gmail.com>
2025-01-29 16:50:10 -0300

    Inclusão dos fontes da aplicação

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new TcpListener(IPAddress.Any, 5000);
+listener.Start();
+
+System.Console.WriteLine("Servidor iniciado. Aguardando conexões...");
+
+while(true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    System.Console.WriteLine("Cliente conectado");
+
+    _ = Task.Run(async () =>
+    {
+        using (var networkStream = client.GetStream())
+        {
+            var buffer = new byte[1024];
+            int bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            System.Console.WriteLine($"Mensagem recebida: {message}");
+        }
+        client.Close();
+    });
+}
