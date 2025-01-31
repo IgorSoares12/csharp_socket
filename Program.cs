b1196cd59244fb73e173ec2007214ab514bbbb6c
igorsoares12 <joseigorsoarespenha@gmail.com>
2025-01-31 15:15:41 -0300

    teste arquivo de log

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
+
+var logFilePath = "/var/lib/jenkins/workspace/teste-pipeline/application.log";
+using var logWriter = new StreamWriter(logFilePath, append: true);
 
 var listener = new TcpListener(IPAddress.Any, 5000);
 listener.Start();
 
-System.Console.WriteLine("Servidor iniciado. Aguardando conexões...");
+logWriter.WriteLine("Servidor iniciado. Aguardando conexões...");
+logWriter.Flush();
 
-while(true)
+while (true)
 {
     var client = await listener.AcceptTcpClientAsync();
-    System.Console.WriteLine("Cliente conectado");
+    logWriter.WriteLine("Cliente conectado");
+    logWriter.Flush();
 
     _ = Task.Run(async () =>
     {
@@ -20,7 +26,8 @@ while(true)
             int bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length);
 
             string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            System.Console.WriteLine($"Mensagem recebida: {message}");
+            logWriter.WriteLine($"Mensagem recebida: {message}");
+            logWriter.Flush();
         }
         client.Close();
     });
